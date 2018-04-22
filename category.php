ae021a80504cfb6dcab7ea9c95f569b020942ed1
Victoria-Sardelli <sardellivicky4@gmail.com>
2018-04-21 23:19:45 -0400

    created new css files for category and translation pages and linked them respectively. added main content div to those pages and cleaned up index page

--- category.php
+++ category.php
@@ -2,9 +2,13 @@
 <?php
   include("includes/top.php");
 ?>
+<link rel="stylesheet" href="stylesheets/category.css">
 <?php
   include("includes/header.php");
 ?>
+<main class="main-content">
+  <h1>Category Page</h1>
+</main>
 <?php
   include("includes/bottom.php");
 ?>
--- translation.php
+++ translation.php
@@ -2,9 +2,13 @@
 <?php
   include("includes/top.php");
 ?>
+<link rel="stylesheet" href="stylesheets/translation.css">
 <?php
   include("includes/header.php");
 ?>
+<main class="main-content">
+  <h1>Translation Page</h1>
+</main>
 <?php
   include("includes/bottom.php");
 ?>

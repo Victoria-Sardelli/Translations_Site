ab58aa9591ec2d1472be749a784a1d96c1c2d789
Victoria-Sardelli <sardellivicky4@gmail.com>
2018-04-21 22:28:01 -0400

    added two new pages and included top, header, and bottom php pages in them

---
+++ category.php
@@ -0,0 +1,10 @@
+<!-- displays list of existing translations for this category -->
+<?php
+  include("includes/top.php");
+?>
+<?php
+  include("includes/header.php");
+?>
+<?php
+  include("includes/bottom.php");
+?>
---
+++ translation.php
@@ -0,0 +1,10 @@
+<!-- displays a translation pair  -->
+<?php
+  include("includes/top.php");
+?>
+<?php
+  include("includes/header.php");
+?>
+<?php
+  include("includes/bottom.php");
+?>

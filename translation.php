42bbd95198afc8635db22c6a5db02b94eb84abbc
Victoria-Sardelli <sardellivicky4@gmail.com>
2018-04-22 22:42:21 -0400

    added php code to translations page and styled it to show requested translation

--- translation.php
+++ translation.php
@@ -7,7 +7,54 @@
   include("includes/header.php");
 ?>
 <main class="main-content">
-  <h1>Translation Page</h1>
+  <?php
+    // get name of category we want
+    $cat_name = "";
+    if(isset($_GET["catName"])) {
+      $cat_name = $_GET["catName"];
+    }
+    // check value of category name
+    if ($cat_name != "Songs" && $cat_name != "Books" && $cat_name != "Poems") {
+      exit("Invalid Category");
+    }
+    // get id of song we want
+    $song_id = -1;
+    if(isset($_GET["id"])) {
+      $song_id = $_GET["id"];
+    }
+    // check to see that song id was set
+    if ($song_id === -1) {
+      exit("I'm sorry, the requested translation cannot be displayed at this time.");
+    }
+
+    // connect to db that contains translations
+    include 'db_connection.php';
+    $conn = openCon();
+    // set db connection to use UTF-8
+    $conn->query("SET NAMES utf8");
+
+    // get specific translation requested using given category and id
+    $sql = "SELECT id, name, author, engText, japText, dateAdded
+      FROM $cat_name WHERE id = $song_id";
+    $result = $conn->query($sql);
+
+    // if the requested translation is found in db, display it
+    if ($result->num_rows === 1) {
+      $row = $result->fetch_assoc();
+      echo "<h1>".$row["name"]."</h1>"; // name of translated work
+      echo "<h3>by ".$row["author"]."</h3>"; // name of author
+      echo "<div class='trans-text-div'>
+              <p class='trans-text'>".nl2br($row["japText"])."</p>"; // japanese text
+      echo "  <p class='trans-text'>".nl2br($row["engText"])."</p>
+            </div>"; // english text
+      echo "<p class='date-added'>Date added: ".date('F j, Y',strtotime($row["dateAdded"]))."</p>"; // date added to db
+    } else {
+      echo "I'm sorry, the requested translation cannot be displayed at this time.";
+    }
+
+    // close connection to database
+    closeCon($conn);
+  ?>
 </main>
 <?php
   include("includes/bottom.php");

eba62f8e7f40357e1b7248b886417f0f1b6303af
Victoria-Sardelli <sardellivicky4@gmail.com>
2018-04-22 21:58:26 -0400

    finished category page php functionality and added styles. changed slightly the link tags in header and category page

--- category.php
+++ category.php
@@ -2,7 +2,7 @@
 <?php
   include("includes/top.php");
 ?>
-<link rel="stylesheet" href="stylesheets/category.css">
+<link rel="stylesheet" href="./stylesheets/category.css">
 <?php
   include("includes/header.php");
 ?>
@@ -10,8 +10,8 @@
   <?php
     // get name of category we want
     $cat_name = "";
-    if(isset($_GET["name"])) {
-      $cat_name = $_GET["name"];
+    if(isset($_GET["catName"])) {
+      $cat_name = $_GET["catName"];
     }
     // check value of category name
     if ($cat_name != "Songs" && $cat_name != "Books" && $cat_name != "Poems") {
@@ -23,13 +23,18 @@
     // connect to db that contains translations
     include 'db_connection.php';
     $conn = openCon();
+    // set db connection to use UTF-8
+    $conn->query("SET NAMES utf8");
     // get all translations for this category
-    $sql = "SELECT id, engText, japText, dateAdded FROM $cat_name";
+    $sql = "SELECT id, name, author, engText, japText, dateAdded FROM $cat_name";
     $result = $conn->query($sql);
+
     if ($result->num_rows > 0) {
       // display link to each translation in this category
       while($row = $result->fetch_assoc()) {
-        echo "There are results";
+        echo '<a class="trans-link"'
+          .' href="./translation.php?catName='.$cat_name
+          .'&id='.$row["id"].'">'.$row["name"].'</a><br>';
       }
     } else {
       echo "No translations added yet. Check back soon!";
--- includes/header.php
+++ includes/header.php
@@ -3,13 +3,13 @@
       <a href="index.php" class="topbar-text">Translation Practice</a>
       <ul class="nav">
         <li class="nav-item">
-          <a href="./category.php?name=Songs">Songs</a>
+          <a href="./category.php?catName=Songs">Songs</a>
         </li>
         <li class="nav-item">
-          <a href="./category.php?name=Books">Books</a>
+          <a href="./category.php?catName=Books">Books</a>
         </li>
         <li class="nav-item">
-          <a href="./category.php?name=Poems">Poems</a>
+          <a href="./category.php?catName=Poems">Poems</a>
         </li>
       </ul>
     </div>

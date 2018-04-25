5af52415a8d2cdf25aa7d150c381cb856d4b3a66
Victoria-Sardelli <sardellivicky4@gmail.com>
2018-04-24 20:00:14 -0400

    changed db connection to use heroku's config vars

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
   function openCon() {
-    $dbhost = "us-cdbr-iron-east-05.cleardb.net";
-    $dbuser = "b476dc90f94a62";
-    $dbpass = "a6f9d8f9";
-    $db = "heroku_0fea3be9dd8f318";
+    $dbhost = getenv("dbhost");
+    $dbuser = getenv("dbuser");
+    $dbpass = getenv("dbpass");
+    $db = getenv("db");
 
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db)
       or die("Connect failed: %s\n". $conn -> error);

58347f32def41bf7ea2fba7ffdf3b18cf69f36e2
Victoria-Sardelli <sardellivicky4@gmail.com>
2018-04-24 20:27:48 -0400

    added meta tag for viewport so that media queries work as expected

--- includes/top.php
+++ includes/top.php
@@ -2,6 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jap-Eng Translations</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
     <link rel="stylesheet" href="./stylesheets/styles.css">

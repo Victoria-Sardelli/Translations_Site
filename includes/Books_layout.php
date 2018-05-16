a78c3ab4f4f5fcf9fc440ddc56b839ce91973752
Victoria-Sardelli <sardellivicky4@gmail.com>
2018-05-16 15:03:20 -0400

    added <a> tags to lang nav items so that they can be tabbed to. retained cursr pointer css for lang nav <li> so that it is clear that you can clik anywhere in the <li> elem to trigger the js to switch languages

--- includes/Books_layout.php
+++ includes/Books_layout.php
@@ -6,8 +6,10 @@
     echo "<h3>by ".$row["author"]."</h3>"; // name of author
     echo "<div class='book-text-div'>";
     echo  "<ul class='lang-nav'>
-            <li id='jap' class='lang-nav-item lang-active' onclick='switchLang(this.id)'>日本語</li>
-            <li id='eng' class='lang-nav-item' onclick='switchLang(this.id)'>English</li>
+            <li id='jap' class='lang-nav-item lang-active' onclick='switchLang(this.id)'>
+              <a href='javascript:void(0);'>日本語</a></li>
+            <li id='eng' class='lang-nav-item' onclick='switchLang(this.id)'>
+              <a href='javascript:void(0);'>English</a></li>
           </ul>"; // nav to switch between languages
     echo " <p class='jap book-text text-active'>".nl2br($row["japText"])."</p>"; // japanese text
     echo " <p class='eng book-text'>".nl2br($row["engText"])."</p>
